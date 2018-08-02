857a177d0ad8b3ac63865da504d68d8b36f18719
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-08-02 18:59:16 -0300

    Filtrar restaurante funcionando

--- js/app.js
+++ js/app.js
@@ -10,39 +10,47 @@ function initMap() {
     position: uluru, map: map
   });
 }
+
 $('.home').hide();
 $(document).ready(function() {
   setTimeout(function () {
   	$('.logo').hide();
     $('.home').show();
   }, 5000);
-
+  var listaIndex = [];
   var names = [];
   var images = [];
   var types = [];
   var descriptions = [];
+
   $.each(restaurantes, function(index, value) {
+    listaIndex.push(index);
     names.push(value.name);
     images.push(value.image);
     types.push(value.type);
     descriptions.push(value.description);
     $(".imagensRestaurantes").append("<img src= " + value.image + " alt=" + value.name + " >");
   });
-  $("#h1Modal").append(names[0]);
-  $('#imgModal').attr('src', images[0]);
-  $('#imgModal').attr('alt', names[0]);
-  $("#typeModal").append("Tipo: " + types[0]);
-  $('#descriptionModal').append("Descrição: " + descriptions[0]);
-});
-// $('.filtrar').click(function () {
-//    var inputValue = $('.termo').val();
-//
-//    $( "li" ).each(function( ) {
-//      if($(this).text() !== inputValue) {
-//        $(this).fadeOut('slow');
-//      }
-//    });
-//  })
+  $('.btnFiltrar').click(function () {
+   var inputValue = $('.textoFiltrar').val();
+   $.each(names, function(index, value) {
+     if(value === inputValue) {
+       $("#h1Modal").append(names[index]);
+       $('#imgModal').attr('src', images[index]);
+       $('#imgModal').attr('alt', names[index]);
+       $('#imgModal').show();
+       $("#typeModal").append("Tipo: " + types[index]);
+       $('#descriptionModal').append("Descrição: " + descriptions[index]);
+     }
+  });
+   $('.close').click(function () {
+     $('.textoFiltrar').val("");
+     $('#h1Modal').html("");
+     $('#imgModal').hide();
+     $('#typeModal').html("");
+     $('#descriptionModal').html("");
+    });
+  })
 //
 //  $('.termo').on('input', function () {
 //    if($(this).val() === "") {
@@ -51,3 +59,4 @@ $(document).ready(function() {
 //      });
 //    }
 //  })
+});
