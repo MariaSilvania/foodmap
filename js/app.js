a0242ea24484b71e4f448910386787945ca24d68
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-08-04 00:57:55 -0300

    alteração no css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 function initMap() {
+  var latitude;
+  var longitude;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14, center: {lat: -23.561751 , lng: -46.658998},
   });
   for (i = 0; i < restaurantes.length; i++) {
-    var latitude = restaurantes[i].latitude;
-    var longitude = restaurantes[i].longitude;
+    latitude = restaurantes[i].latitude;
+    longitude = restaurantes[i].longitude;
     var marker = new google.maps.Marker({
      position: new google.maps.LatLng(latitude , longitude),
      map: map,
     });
   }
 }
+
 $('.home').hide();
 $(document).ready(function() {
   setTimeout(function () {
@@ -30,6 +33,7 @@ $(document).ready(function() {
     descriptions.push(value.description);
     $(".imagensRestaurantes").append("<img src= " + value.image + " alt=" + value.type.toUpperCase() + " val=" + index + " data-toggle='modal' data-target='#modalRestautante'>");
   });
+
   $('.btnFiltrar').click(function () {
      var inputValue = $('.textoFiltrar').val().toUpperCase();
      $("img").each(function( ) {
@@ -47,24 +51,17 @@ $(document).ready(function() {
      })
      $('img').click(function () {
        var index = $(this).attr("val");
-       console.log(index);
-       // $.each(names, function(index, value) {
-       //   if(value === imgValue) {
-           $("#h1Modal").append(names[index]);
-           $('#imgModal').attr('src', images[index]);
-           $('#imgModal').attr('alt', names[index]);
-           $('#imgModal').show();
-           $("#typeModal").append("<b>Tipo: </b>" + types[index]);
-           $('#descriptionModal').append("<b>Descrição: </b>" + descriptions[index]);
-       //   }
-       // })
+         $("#h1Modal").append(names[index]);
+         $('#imgModal').attr('src', images[index]);
+         $('#imgModal').attr('alt', names[index]);
+         $('#imgModal').show();
+         $("#typeModal").append("<b>Tipo: </b>" + types[index]);
+         $('#descriptionModal').append("<b>Descrição: </b>" + descriptions[index]);
     });
    $('.close').click(function () {
-     $('.textoFiltrar').val("");
      $('#h1Modal').html("");
      $('#imgModal').hide();
      $('#typeModal').html("");
      $('#descriptionModal').html("");
     });
-
 });
